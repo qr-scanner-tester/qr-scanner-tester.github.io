fe05522d90697bd48e09bd2e5754f915a37d1a2f
Anton Lenok <dzx912@gmail.com>
2024-07-20 19:04:36 +0300

    Add first interface

---
+++ src/mainApp.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    tWabApp = window.Telegram.WebApp
+    const outputTextMessage = document.getElementById("outputMessage");
+
+    init();
+
+    function init() {
+        tWabApp.MainButton.setText("Scan QR code");
+        tWabApp.onEvent('mainButtonClicked', showQRScanner);
+        tWabApp.onEvent('qrTextReceived', processQRCode);
+    }
+
+    function showQRScanner() {
+        // Sets QR message
+        let par = {
+            text: ""
+        };
+        tWabApp.showScanQrPopup(par);
+    }
+
+    function processQRCode(data) {
+        outputTextMessage.value = data.data
+        tWabApp.closeScanQrPopup();
+    }
+});
